c625087dbaf80be757c83dd3d9898fc20ecac4e0
Aaron Montgomery <montaaron11@gmail.com>
2020-08-05 08:55:11 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,10 +4,7 @@
 def key_for_min_value(name_hash)
   name_hash.each do |name, value| 
     if value < 17
-    puts name 
+      puts name 
+    end
   end
-  elsif value < 2 
-  puts name 
-end
-end
 end
